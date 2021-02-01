cabf121994510e2d3fc8f6470f1b58c24621ca07
Anita Ganti <65381336+anitapeppercorn@users.noreply.github.com>
2021-01-31 17:33:16 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -115,7 +115,7 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(4242, () => console.log(`Node server listening on port ${$4242}!`));
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
 
 
 function checkEnv() {
